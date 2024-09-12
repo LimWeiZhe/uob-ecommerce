64e389c19b6bb74646b6002b7c02e0293ad223a5
LimWeiZhe <ict_limweizhe@connect.np.edu.sg>
2024-09-12 04:02:47 +0000

    added stylesheet and ahtml for about-us

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+// the @controller annotation tells SpringBoot that this is a controller that contains routes
+// Springboot will automatically go through all the methods marked as routes and register them
+@Controller
+public class HomeController {
+
+    // When the user goes to the / URL on the server, call this method
+    @GetMapping("/")
+    // Tell Spring Boot this method returns a HTTP response
+    @ResponseBody
+    public String helloWorld(){
+        return "<h1>Hello World</h1>";
+    }
+
+    @GetMapping("/about-us")
+    // if the route is not marked with @responsebody, then by default we are using templates
+    // the 'Model model' parameter is automatically passed to aboutUs when is called by Spring
+    // as the 'view model' and it also allows us to inject variables into our template
+    public String aboutUs(Model model){
+        // we need to return the file path to the template
+        // relative to resources/templates
+
+        // get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        //the view model is automaticaly passed to the template and
+        // any attributes in it awill be available as variables
+        model.addAttribute("currentDateTime", currentDateTime);
+
+        
+        return "about-us";
+    }
+
+    @GetMapping("/contact-us")
+    public String contactUs(){
+        return "contact-us";
+    }
+}
